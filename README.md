05abdad519d4fc93fea3ca6dbb84477dfe41d7af
Tokiwadai-Railgun <arthur.scheidbach@live.fr>
2024-10-29 22:47:27 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Features
+* [ ] Gestion des comptes 
+* [ ] Gestion des dockers
+* [ ] Gestion des process
+    * [ ] Izuna
+* [ ] VMS (Potentiellement)
+* [ ] Cloud
+* [ ] IA
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1>Welcome to SvelteKit</h1>
+<p class="test">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style>
+	.test {
+		color: #000;
+	}
+</style>
