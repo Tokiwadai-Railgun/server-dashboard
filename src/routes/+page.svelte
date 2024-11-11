bb5457ce4a29ab52a4cb8ea16f28c72f731cb168
Tokiwadai-Railgun <arthur.scheidbach@live.fr>
2024-11-11 09:42:00 +0100

    Removed one of the two markdown renderer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="test">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<style>
-	.test {
-		color: #000;
-	}
-</style>
+<h1>Welcome to the server homepage</h1>
