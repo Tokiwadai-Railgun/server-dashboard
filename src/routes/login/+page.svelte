5a5d897c9ba90638abde03ca05f4815502ff8b51
Fuyuki <arthur.scheidbach@proton.me>
2025-01-18 13:47:25 +0100

    starting changes on

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,8 @@
 <h1>Login</h1>
+
+<!-- Generate the login page: -->
+
+<form action="">
+		<input type="text" name="username" placeholder="Username" required>
+		<input type="password" name="password" placeholder="Password" required>
+</form>
