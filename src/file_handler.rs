ca48e70a475d12b4fe4f8a98f2192e13a6bfd5a3
Tokiwadai-Railgun <arthur.scheidbach@proton.me>
2025-03-16 11:28:15 +0100

    Finished upload

--- src/file_handler.rs
+++ src/file_handler.rs
@@ -1,55 +1,56 @@
-use actix_web::{self, get, post, web, HttpRequest, HttpResponse, Responder};
+use actix_multipart::form;
+use actix_multipart::form::{json::Json, tempfile::TempFile, MultipartForm};
+use actix_web::{self, get, post, HttpRequest, HttpResponse, Responder};
+use serde::Deserialize;
 use std::fs::File;
 use std::path::Path;
 use std::fs;
-use serde::{Serialize, Deserialize};
 use std::io::prelude::*;
 
 const BASE_PATH: &str = "/home/fuyuki/Documents/server-dashboard-api-storage/storage";
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, MultipartForm)]
 pub struct FileData {
-    user_data: UserdData,
-    file_name: String,
-    file_content: String,
+    file: TempFile,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct UserdData {
-    user_id: i16,
-    token: String
-}
 
 #[get("/file")]
-pub async fn get_file(request: HttpRequest) -> impl Responder {
+pub async fn get_file() -> impl Responder {
     HttpResponse::InternalServerError().body("Endpoint not implemented yet")
 }
 
 #[post("/upload")]
-pub async fn upload_file(request_body: web::Json<FileData>) -> impl Responder{
-    //TODO: Verify account informations
+pub async fn upload_file( request: HttpRequest, MultipartForm(form): MultipartForm<FileData>) -> impl Responder{
+    let user_id = request.headers().get("user_id").ok_or_else(|| HttpResponse::BadRequest().body("Missing user_id header")); 
 
-
-    // if all is fine then send request
-    match save_file(request_body).await {
-        Ok(_) => {
-            HttpResponse::Ok().body("File created successfully")
-        },
-        Err(e) => {
-            println!("{}", e);
-            HttpResponse::InternalServerError().body("Error occured during file creation, please retry")//
-        }
+    if let Err(response) = user_id {
+        return response;
     }
-}
 
-async fn save_file(file_data: actix_web::web::Json<FileData>) -> std::io::Result<()> {
-    // verify if user directory exists, if not then create it 
-    if !Path::new(format!("{}/{}", BASE_PATH, file_data.user_data.user_id).as_str()).exists() {
-        fs::create_dir(format!("{}/{}", BASE_PATH, file_data.user_data.user_id))?;
+    let user_id = user_id.unwrap().to_str().unwrap();
+
+    // ----- Préparation du dossier de sauvegarde -----
+    if !Path::new(format!("{}/{}", BASE_PATH, user_id).as_str()).exists() {
+        if let Err(e) = fs::create_dir(format!("{}/{}", BASE_PATH, user_id)) {
+            println!("Error creating file: {}", e);
+            return HttpResponse::InternalServerError().body("Error creating user directory");
+        };
         println!("Folder created");
     }
-    let path = format!("{}/{}/{}", BASE_PATH, file_data.user_data.user_id, file_data.file_name);
-    let mut file = File::create(path)?;
-    file.write_all(file_data.file_content.as_bytes())?;
-    Ok(())
+
+    // ----- Copie du fichier temporaire à la bonne destination -----
+    let path = format!("{}/{}/{}", BASE_PATH, user_id, form.file.file_name.unwrap());
+    let temp_path = form.file.file.path();
+
+    match fs::copy(temp_path, path) {
+        Ok(_) => {
+            println!("File created ! ");
+            HttpResponse::Ok().body("File created")
+        },
+        Err(e) => {
+            println!("Error creating file : {}", e);
+            HttpResponse::InternalServerError().body("An error occured when saving the file")
+        }
+    }
 }
